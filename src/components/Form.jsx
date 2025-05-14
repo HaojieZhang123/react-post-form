fdcdcc7e80c825537461a6762ef2be6ed632faa3
HaojieZhang123 <haojiezhang123@gmail.com>
2025-05-14 16:15:46 +0200

    [UPD] form stylized

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,7 +6,7 @@ const Form = ({ formData, handleChange, handleSubmit }) => {
             <div className="form-container">
 
                 <div className='form-header'>
-                    <h1 className='white-text'>Create a new repository</h1>
+                    <h2 className='white-text'>Create a new repository</h2>
                     <p>
                         A repository contains all project files, including the revision history. Already have a project repository elsewhere?
                         <span className='link'>
@@ -22,17 +22,17 @@ const Form = ({ formData, handleChange, handleSubmit }) => {
                     {/* author and title */}
                     <div className='d-flex col-100 gap'>
                         <div className="col-50 form-half-row">
-                            <label htmlFor="author"><b className='white-text'>Owner*</b></label>
+                            <label htmlFor="author"><b className='white-text'>Author*</b></label>
                             <input type="text" id="author" name="author" value={formData.author} onChange={handleChange} />
                         </div>
 
                         <div className="col-50 form-half-row">
-                            <label htmlFor="title"><b className='white-text'>Repository name</b></label>
+                            <label htmlFor="title"><b className='white-text'>Title*</b></label>
                             <input type="text" id="title" name="title" value={formData.title} onChange={handleChange} />
                         </div>
                     </div>
 
-                    <p><b className='white-text'>Great repository names are short and memorable. Need inspiration? How about <span className='green-text'>urban-barnacles</span> ?</b></p>
+                    <p className='white-text'>Great repository names are short and memorable. Need inspiration? How about <b className='green-text'>urban-barnacles</b> ?</p>
 
                     <hr />
 
@@ -46,10 +46,14 @@ const Form = ({ formData, handleChange, handleSubmit }) => {
                     <hr />
 
                     {/* public */}
-                    <div>
+                    <div className='checkbox-area'>
                         <input type="checkbox" id="public" name="public" checked={formData.public} onChange={handleChange}></input>
                         <label htmlFor="public">
-                            Public<br />
+                            <span className='white-text'>
+                                <i class="fa-solid fa-book-bookmark"></i>
+                                Public
+                            </span>
+                            <br />
                             Anyone on the internet can see this repository. You choose who can commit.
                         </label>
                     </div>
@@ -65,7 +69,9 @@ const Form = ({ formData, handleChange, handleSubmit }) => {
                     <hr />
 
                     {/* submit */}
-                    <button type="submit" onClick={handleSubmit}>Submit</button>
+                    <div className='submit-area'>
+                        <button type="submit" onClick={handleSubmit}>Send form</button>
+                    </div>
                 </form>
             </div>
         </>
